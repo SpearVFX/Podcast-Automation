d4da871f566a8e7ad725d639da7425cbb9d89cb0
Sasho Kostov <alexander.kostov37@gmail.com>
2023-03-07 22:45:00 +0200

    Add utility.h and a lot of changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,34 @@
 #include "../third-party/AudioFile.h"
 #include "../third-party/lodepng.h"
+#include "utility.h"
 #include <string>
 #include <iostream>
 #include <chrono>
-#include <windows.h>
-
-// Macro defining left focus
-#define LEFT 0
-// Macro defining right focus
-#define RIGHT 1
-// Macro defining central focus
-#define CENTRAL 2
-
-std::vector<unsigned char> generateSolidColorPixels(unsigned width, unsigned height, unsigned r = 0, unsigned g = 0, unsigned b = 0, unsigned a = 255) {
-	std::vector<unsigned char> image;
-	image.resize(width * height * 4);
-	for (unsigned y = 0; y < height; y++) {
-		for (unsigned x = 0; x < width; x++) {
-			image[4 * width * y + 4 * x + 0] = r;
-			image[4 * width * y + 4 * x + 1] = g;
-			image[4 * width * y + 4 * x + 2] = b;
-			image[4 * width * y + 4 * x + 3] = a;
-		}
-	}
-	return image;
-}
-
-// These operations would be done for every frame, so we pre-generate all the solid color images
-std::vector<unsigned char> redPixels = generateSolidColorPixels(64, 64, 255);
-std::vector<unsigned char> greenPixels = generateSolidColorPixels(64, 64, 0, 255);
-std::vector<unsigned char> bluePixels = generateSolidColorPixels(64, 64, 0, 0, 255);
 
 
-// Encode an array of pixels into a png image.
-void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
-	//Encode the image
-	unsigned error = lodepng::encode(filename, image, width, height);
-	
-	//if there's an error, display it
-	if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-}
+//TODO: Make it work for single camera and 3 camera setup, cuz now either one is broken.
+// Add all ffmpeg commands
 
-bool isSpeaking(double averageDB) {
-	return averageDB >= 2.0f;
-}
 
-void printLogo() {
-	std::string logoAscii =
-"\n \n \n\
-          #%%%%%%%%%%%%%%%# .#%%%%%%%* #%%%%%%%%%%%%%%%%#.\n\
-          *%%%%%%%##**+==-...=*%%%%%%%.+%%%%%%%%%%%%%%%%%#\n\
-          *%%%*....:-=++*#%%#* :%%%%%%-:%%%%%%%%%##*%%%%%#\n\
-          +%%%###%%%%%%%%%%%%*  #%%%%%* #%%%%%+.   -%%%%#=              Podcast automation script\n\
-          +%%%%%%%%%%%%%%%%%#- :#%%%%%# +%%%%%*:::+#%%#=..                written by: Sasho Prostoff\n\
-          =%%%%-::-+%%%%%%%+.:*%%%%%%%%::%%%%%%%%%%%#- -**\n\
-          =%%%%.  =%%%%%%*::*%%%#+#%%%%+ #%%%%%%%%*- -#%%*                                Barlog 2022\n\
-          -%%%%..*%%%%%*:.+%%%*--#%%%%%# +%%%%%#+: =#%%%%*\n\
-          -%%%%%%%%%%#-.+#%#+:.+%%%%%%%%.-%%%%%..=#%%%%%%*\n\
-          .#%%%%%%%#=.+#%%%=.=#%%%#*%%%%-.%%%%%-=%%%%%%%%*\n\
-            :=+++*= :#%%%%%%%%%%*: -%%%%+ #%%%%=:%%%%%%%%*\n\
-          =*++===-: -#%%%%%%%#=..  .=+*## *%%%%+ %%%%%%%#=\n\
-          -%%%%%%%%*- -#%%%*::=#%%##*+=-:.:=*#%# #%%%%%*  \n\
-          :%%%%%%%%%%#- -: :*%%%%%%%%%%%%%%#+:.- +%%%%%*  \n\
-          .%%%%%=*%%%%%#: -%%%%%%%%%%%%%%%%%%%#+.=%%%%%*  \n\
-           %%%%%+ #%%%%%%= +%%%%%*=++*#%%%%%%%%%-=%%%%%*                Automates the proccess of\n\
-           #%%%%%-.#%%%%%%+ =%%%%#=:.    .-#%%%%=-%%%%%*                editing a three-camera set up.\n\
-           #%%%%%#.:#%%%%%%*.:#%%%%%%#*=+#%%%%%%=:%%%%%*  \n\
-           %%%%%%%* =%%%%%%%#:.*%%%%%%%%%%%%%%%%=:%%%%%*  \n\
-          :%%%%%%%%= +%%%%%%%#- =%%%%%%%%%%%%%%#-.%%%%%*  \n\
-          =%%%%%%%%%: *%%%%%%%%+ :#%%%%%#####**.  *****+\n";
-	for (char a : logoAscii) {
-		if (a == '%') {
-			std::cout << "\033[1;31m%\033[0m";
-		}
-		else {
-			std::cout << a;
-		}
-	}
-}
-
-int main() {
-	printLogo();
-	//return 0;
+void exportVideoTemplate(std::string audioFileName, int cameraCount = 3) {
+	/* Keeping this, so I can just copy paste when I need performance check.
 	auto start = std::chrono::steady_clock::now();
 	auto end = std::chrono::steady_clock::now();
 	std::cout << "Elapsed time in milliseconds: "
 		<< std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
 		<< " ms" << std::endl;
-
+		*/
+	if (cameraCount != 1 && cameraCount != 3) {
+		std::cout << "INVALID NUMBER OF CAMERAS! 1 and 3 cameras are supported." << std::endl;
+	}
 	AudioFile<double> audioFile;
-	//AudioFile<double> audioFile2;
-	//audioFile.load("../res/left-right.wav", false);
-	//audioFile.load("../res/episode-1.wav", true);
-	audioFile.load("C:/Users/Kostov/Videos/BARLOG/Podcasts/Episode 2/wORLD cUP/Audio/Processed/Barlog LR Worllld Cup Mixdown 1.wav", true);
-	//audioFile.load("C:/Users/Kostov/Videos/BARLOG/Podcasts/Episode 1/MIC1.wav", true);
+	audioFile.load(audioFileName, true); // For podcasts the load should always be true (note: it's my custom functianality in AudioFile.h)
+
+
 	int sampleRate = audioFile.getSampleRate();
 	int bitDepth = audioFile.getBitDepth();
 
-	int numSamples = audioFile.demandSamples.numSamples;
+	int numSamples = audioFile.demandSamples.numSamples; // Demand samples is practically the count of all samples, not just the current demand.
 	double lengthInSeconds = audioFile.getLengthInSeconds();
 
 	int numChannels = audioFile.getNumChannels();
@@ -111,21 +42,21 @@ int main() {
 	double averageDBLeft = 0;
 	double averageDBRight = 0;
 	std::vector<std::vector<double>> averagedDB;
-	averagedDB.resize(2);
+	averagedDB.resize(cameraCount);
 	for (int i = 0; i < numSamples; i++)
 	{
 		double currentSampleLeft = audioFile.demandSamples.getBufferAt(LEFT, i);
 		double currentSampleRight = audioFile.demandSamples.getBufferAt(RIGHT, i);
+		
 		averageDBLeft += currentSampleLeft * currentSampleLeft;
 		averageDBRight += currentSampleRight * currentSampleRight;
 		if (hzElapsed++ == sampleRate) {
+			// todo: better approach to noise calculation.
 			double calcLeft = (averageDBLeft / sampleRate) * 1000;
 			double calcRight = (averageDBRight / sampleRate) * 1000;
 			averagedDB[LEFT].push_back(calcLeft);
 			averagedDB[RIGHT].push_back(calcRight);
 			std::string result;
-			//result += " Average DbS: ";
-			//result += std::to_string(calc);
 			result += " Seconds elapsed: ";
 			result += std::to_string(secondsElapsed++);
 
@@ -138,109 +69,154 @@ int main() {
 	secondsElapsed = 0;
 	// 0 - left, 1 - right, 2 - central
 	int cameraFocus = CENTRAL;
+	int skipFrames = 0; // These are used for multicamera
+	//todok: refactor this...
 
-	int skipFrames = 0;
+	std::wstring exportDirectoryWide = std::wstring(audioFileName.begin(), audioFileName.begin() + audioFileName.find('.'));
+	std::string exportDirectory = std::string(audioFileName.begin(), audioFileName.begin() + audioFileName.find('.'));
+	std::string fileNameRoot = exportDirectory.substr(exportDirectory.find_last_of('\\') + 1);
+	_wmkdir(exportDirectoryWide.c_str());
+	if (cameraCount == 1) {
 
-	for (size_t i = 2; i < averagedDB[0].size(); i++)
-	{
-		std::string filename = "test_" + std::to_string(secondsElapsed++) + ".png";
-		std::cout << filename << std::endl;
-		switch (cameraFocus) {
-		case(LEFT): encodeOneStep(filename.c_str(), redPixels, 64, 64); break;
-		case(RIGHT): encodeOneStep(filename.c_str(), greenPixels, 64, 64); break;
-		case(CENTRAL): encodeOneStep(filename.c_str(), bluePixels, 64, 64); break;
+		for (size_t i = 2; i < averagedDB[0].size(); i++) {
+			std::string filename = exportDirectory + "/" + fileNameRoot + "_" + std::to_string(secondsElapsed++) + ".png";
+			bool isCurrentlySpeaking = isSpeaking(averagedDB[LEFT][i]);
+			if (isCurrentlySpeaking) {
+				encodeOneStep(filename.c_str(), greenPixels, 64, 64);
+			} else {
+				encodeOneStep(filename.c_str(), blackPixels, 64, 64);
+			}
 		}
-		if (skipFrames > 0) {
-			skipFrames--;
-			continue;
-		}
-		double lDb = averagedDB[LEFT][i];
-		double rDb = averagedDB[RIGHT][i];
-		bool isLeftSpeaking = isSpeaking(averagedDB[LEFT][i]);
-		bool isRightSpeaking = isSpeaking(averagedDB[RIGHT][i]);
-
-		bool isLeftSpeakingNext3Sec = isSpeaking((averagedDB[LEFT][i - 1] + averagedDB[LEFT][i - 2])/2.0f);
-		bool isRightSpeakingNext3Sec = isSpeaking((averagedDB[RIGHT][i - 1] + averagedDB[RIGHT][i - 2])/2.0f);
-
-
-
-
-		if ((isLeftSpeakingNext3Sec && isRightSpeakingNext3Sec) || (!isLeftSpeakingNext3Sec && !isRightSpeakingNext3Sec)) {
-			cameraFocus = CENTRAL;
-			continue;
-		}
-
-		if (cameraFocus == CENTRAL) {
-			if (isLeftSpeakingNext3Sec) {
-				cameraFocus = LEFT;
-				skipFrames += 3;
-			}
-			else if (isRightSpeakingNext3Sec) {
-				cameraFocus = RIGHT;
-				skipFrames += 3;
-			}
-			else {
-				skipFrames += 1;
-			}
-			continue;
-		}
-
-		if (cameraFocus == LEFT) {
-			if (isRightSpeakingNext3Sec && !isLeftSpeakingNext3Sec) {
-				cameraFocus = RIGHT;
-				skipFrames += 1;
-			}
-			else if (isLeftSpeaking)
-				cameraFocus = LEFT;
-			else {
-				cameraFocus = CENTRAL;
-				skipFrames += 2;
-			}
-			continue;
-		}
-
-		if (cameraFocus == RIGHT) {
-			if (isLeftSpeakingNext3Sec && !isRightSpeakingNext3Sec) {
-				cameraFocus = LEFT;
-				skipFrames += 1;
-			}
-			else if (isRightSpeaking)
-				cameraFocus = RIGHT;
-			else {
-				cameraFocus = CENTRAL;
-				skipFrames += 2;
-			}
-			continue;
-		}
-
-
-
-
-
-		// if speaking not on cameraFocus AND will speak for more than 2 seconds AND 
-		// the current speaker stops speaking for the following 2 seconds 
-		// focus on new target.
-		// ELSE focus on central.
-
-		// if both sides speak in 2 second interval 
-		// focus on central
-
-		// if no one is speaking for 2 seconds =/= (combine with above)
-
-		
-		//if (isLeftSpeaking && isRightSpeaking)
-		//	encodeOneStep(filename.c_str(), bluePixels, 64, 64);
-		//else if (isLeftSpeaking)
-		//	encodeOneStep(filename.c_str(), redPixels, 64, 64);
-		//else if (isRightSpeaking)
-		//	encodeOneStep(filename.c_str(), greenPixels, 64, 64);
-		//else
-		//	encodeOneStep(filename.c_str(), bluePixels, 64, 64);
 	}
-	std::cout << "DONE!!!!" << std::endl;
+	else {
+		for (size_t i = 3; i < averagedDB[0].size(); i++)
+		{
+			std::string filename = exportDirectory + "/" + fileNameRoot + "_" + std::to_string(secondsElapsed++) + ".png";
+			//std::cout << filename << std::endl;
+			switch (cameraFocus) {
+			case(LEFT): encodeOneStep(filename.c_str(), redPixels, 64, 64); break;
+			case(RIGHT): encodeOneStep(filename.c_str(), greenPixels, 64, 64); break;
+			case(CENTRAL): encodeOneStep(filename.c_str(), bluePixels, 64, 64); break;
+			}
+			if (skipFrames > 0) {
+				skipFrames--;
+				continue;
+			}
+			double lDb = averagedDB[LEFT][i];
+			double rDb = averagedDB[RIGHT][i];
+			bool isLeftSpeaking = isSpeaking(averagedDB[LEFT][i]);
+			bool isRightSpeaking = isSpeaking(averagedDB[RIGHT][i]);
 
+			bool isLeftSpeakingNext3Sec = isSpeaking((averagedDB[LEFT][i - 1] + averagedDB[LEFT][i - 2] + averagedDB[LEFT][i - 3]) / 3.0f);
+			bool isRightSpeakingNext3Sec = isSpeaking((averagedDB[RIGHT][i - 1] + averagedDB[RIGHT][i - 2] + averagedDB[RIGHT][i - 3]) / 3.0f);
+
+
+
+
+			if ((isLeftSpeakingNext3Sec && isRightSpeakingNext3Sec) || (!isLeftSpeakingNext3Sec && !isRightSpeakingNext3Sec)) {
+				cameraFocus = CENTRAL;
+				continue;
+			}
+
+			if (cameraFocus == CENTRAL) {
+				if (isLeftSpeakingNext3Sec) {
+					cameraFocus = LEFT;
+					skipFrames += 3;
+				}
+				else if (isRightSpeakingNext3Sec) {
+					cameraFocus = RIGHT;
+					skipFrames += 3;
+				}
+				else {
+					skipFrames += 1;
+				}
+				continue;
+			}
+
+			if (cameraFocus == LEFT) {
+				if (isRightSpeakingNext3Sec && !isLeftSpeakingNext3Sec) {
+					cameraFocus = RIGHT;
+					skipFrames += 1;
+				}
+				else if (isLeftSpeaking)
+					cameraFocus = LEFT;
+				else {
+					cameraFocus = CENTRAL;
+					skipFrames += 2;
+				}
+				continue;
+			}
+
+			if (cameraFocus == RIGHT) {
+				if (isLeftSpeakingNext3Sec && !isRightSpeakingNext3Sec) {
+					cameraFocus = LEFT;
+					skipFrames += 1;
+				}
+				else if (isRightSpeaking)
+					cameraFocus = RIGHT;
+				else {
+					cameraFocus = CENTRAL;
+					skipFrames += 2;
+				}
+				continue;
+			}
+
+
+
+
+
+			// if speaking not on cameraFocus AND will speak for more than 2 seconds AND 
+			// the current speaker stops speaking for the following 2 seconds 
+			// focus on new target.
+			// ELSE focus on central.
+
+			// if both sides speak in 2 second interval 
+			// focus on central
+
+			// if no one is speaking for 2 seconds =/= (combine with above)
+
+
+			//if (isLeftSpeaking && isRightSpeaking)
+			//	encodeOneStep(filename.c_str(), bluePixels, 64, 64);
+			//else if (isLeftSpeaking)
+			//	encodeOneStep(filename.c_str(), redPixels, 64, 64);
+			//else if (isRightSpeaking)
+			//	encodeOneStep(filename.c_str(), greenPixels, 64, 64);
+			//else
+			//	encodeOneStep(filename.c_str(), bluePixels, 64, 64);
+		}
+}
+	std::cout << "Exported all images for " << audioFileName << "." << std::endl;
+
+	std::cout << "Creating video using ffmpeg" << audioFileName << "." << std::endl;
+	std::string program = "C:/Program Files/ffmpeg/ffmpeg.exe";
+	std::string commandLineExe;
+	// ffmpeg should be a command
+	commandLineExe +=  " -r 1 -i \"" + exportDirectory + "\\" + fileNameRoot + "_%d.png" + "\" -c:v libx264 -vf fps=25 -pix_fmt yuv420p -y " + exportDirectory + "\\" + fileNameRoot + "_" + "output.mp4";
+	std::cout << "Calling program: " + program + " with arguments: " + commandLineExe << std::endl;
+	startupCmdProcess("C:/Program Files/ffmpeg/ffmpeg.exe", (char*)commandLineExe.c_str());
+	cmdProcessFinished = false;
 	// or, just use this quick shortcut to print a summary to the console
-	audioFile.printSummary();
+	//audioFile.printSummary();
+}
+
+int main(int argc, char* argv[]) {
+	//printLogo();
+	if (argc < 2) {
+		std::cout << " You need to pass a .WAV file(s) as an argument" << std::endl;
+	} else if (argc == 2) {
+		exportVideoTemplate(argv[1], 3);
+	}
+	if (argc > 2) {
+		if (isdigit(atoi(argv[1]))) {
+			int cameraCount = atoi(argv[1]);
+			for (int i = 2; i < argc; i++) {
+				exportVideoTemplate(argv[i], cameraCount);
+			}
+		}
+	}
+	//return 0;
+	
 
 	return 0;
 }
---
+++ src/utility.h
@@ -0,0 +1,195 @@
+#pragma once
+#include <Windows.h>
+/*void startupCmdProcess(LPCSTR lpApplicationName, char* args)
+{
+	// additional information
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+
+	// set the size of the structures
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	// start the program up
+	CreateProcessA
+	(
+		lpApplicationName,   // the path
+		args,                // Command line
+		NULL,                   // Process handle not inheritable
+		NULL,                   // Thread handle not inheritable
+		FALSE,                  // Set handle inheritance to FALSE
+		CREATE_NEW_CONSOLE,     // Opens file in a separate console
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi           // Pointer to PROCESS_INFORMATION structure
+	);
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
+*/
+HANDLE m_hChildStd_OUT_Rd = NULL;
+HANDLE m_hChildStd_OUT_Wr = NULL;
+HANDLE m_hreadDataFromExtProgram = NULL;
+const int BUFSIZE = 10000;
+static bool cmdProcessFinished = false;
+
+DWORD __stdcall readDataFromExtProgram(void* argh)
+{
+    DWORD dwRead;
+    CHAR chBuf[BUFSIZE];
+    BOOL bSuccess = FALSE;
+
+    for (;;)
+    {
+        bSuccess = ReadFile(m_hChildStd_OUT_Rd, chBuf, BUFSIZE, &dwRead, NULL);
+        if (!bSuccess || dwRead == 0) continue;
+
+        for (char c : chBuf) {
+            if (c >= 0) {
+                std::cout << c;
+            }
+        }
+        if (!bSuccess) break;
+    }
+    cmdProcessFinished = true;
+    return 0;
+}
+
+bool startupCmdProcess(std::string externalProgram, std::string arguments)
+{
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+    SECURITY_ATTRIBUTES saAttr;
+
+    ZeroMemory(&saAttr, sizeof(saAttr));
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
+
+    // Create a pipe for the child process's STDOUT. 
+
+    if (!CreatePipe(&m_hChildStd_OUT_Rd, &m_hChildStd_OUT_Wr, &saAttr, 0))
+    {
+        // log error
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    // Ensure the read handle to the pipe for STDOUT is not inherited.
+
+    if (!SetHandleInformation(m_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0))
+    {
+        // log error
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    si.hStdError = m_hChildStd_OUT_Wr;
+    si.hStdOutput = m_hChildStd_OUT_Wr;
+    si.dwFlags |= STARTF_USESTDHANDLES;
+
+    ZeroMemory(&pi, sizeof(pi));
+
+    std::string commandLine = externalProgram + " " + arguments;
+    // Start the child process. 
+    if (!CreateProcessA(NULL,           // No module name (use command line)
+        (LPSTR)(commandLine.c_str()),    // Command line
+        NULL,                           // Process handle not inheritable
+        NULL,                           // Thread handle not inheritable
+        TRUE,                           // Set handle inheritance
+        0,                              // No creation flags
+        NULL,                           // Use parent's environment block
+        NULL,                           // Use parent's starting directory 
+        &si,                            // Pointer to STARTUPINFO structure
+        &pi)                            // Pointer to PROCESS_INFORMATION structure
+        )
+    {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+    else
+    {
+        m_hreadDataFromExtProgram = CreateThread(0, 0, readDataFromExtProgram, NULL, 0, NULL);
+    }
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    return S_OK;
+}
+
+/**/
+
+// Macro defining left focus
+#define LEFT 0
+// Macro defining right focus
+#define RIGHT 1
+// Macro defining central focus
+#define CENTRAL 2
+
+std::vector<unsigned char> generateSolidColorPixels(unsigned width, unsigned height, unsigned r = 0, unsigned g = 0, unsigned b = 0, unsigned a = 255) {
+	std::vector<unsigned char> image;
+	image.resize(width * height * 4);
+	for (unsigned y = 0; y < height; y++) {
+		for (unsigned x = 0; x < width; x++) {
+			image[4 * width * y + 4 * x + 0] = r;
+			image[4 * width * y + 4 * x + 1] = g;
+			image[4 * width * y + 4 * x + 2] = b;
+			image[4 * width * y + 4 * x + 3] = a;
+		}
+	}
+	return image;
+}
+
+// These operations would be done for every frame, so we pre-generate all the solid color images
+std::vector<unsigned char> redPixels = generateSolidColorPixels(64, 64, 255);
+std::vector<unsigned char> greenPixels = generateSolidColorPixels(64, 64, 0, 255);
+std::vector<unsigned char> bluePixels = generateSolidColorPixels(64, 64, 0, 0, 255);
+std::vector<unsigned char> blackPixels = generateSolidColorPixels(64, 64, 0, 0, 0);
+
+
+// Encode an array of pixels into a png image.
+void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+	//Encode the image
+	unsigned error = lodepng::encode(filename, image, width, height);
+
+	//if there's an error, display it
+	if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+}
+
+bool isSpeaking(double averageDB) {
+	return averageDB >= 0.7f;
+}
+
+void printLogo() {
+	std::string logoAscii =
+		"\n \n \n\
+          #%%%%%%%%%%%%%%%# .#%%%%%%%* #%%%%%%%%%%%%%%%%#.\n\
+          *%%%%%%%##**+==-...=*%%%%%%%.+%%%%%%%%%%%%%%%%%#\n\
+          *%%%*....:-=++*#%%#* :%%%%%%-:%%%%%%%%%##*%%%%%#\n\
+          +%%%###%%%%%%%%%%%%*  #%%%%%* #%%%%%+.   -%%%%#=              Podcast automation script\n\
+          +%%%%%%%%%%%%%%%%%#- :#%%%%%# +%%%%%*:::+#%%#=..                written by: Sasho Prostoff\n\
+          =%%%%-::-+%%%%%%%+.:*%%%%%%%%::%%%%%%%%%%%#- -**\n\
+          =%%%%.  =%%%%%%*::*%%%#+#%%%%+ #%%%%%%%%*- -#%%*                                Barlog 2023\n\
+          -%%%%..*%%%%%*:.+%%%*--#%%%%%# +%%%%%#+: =#%%%%*\n\
+          -%%%%%%%%%%#-.+#%#+:.+%%%%%%%%.-%%%%%..=#%%%%%%*\n\
+          .#%%%%%%%#=.+#%%%=.=#%%%#*%%%%-.%%%%%-=%%%%%%%%*\n\
+            :=+++*= :#%%%%%%%%%%*: -%%%%+ #%%%%=:%%%%%%%%*\n\
+          =*++===-: -#%%%%%%%#=..  .=+*## *%%%%+ %%%%%%%#=\n\
+          -%%%%%%%%*- -#%%%*::=#%%##*+=-:.:=*#%# #%%%%%*  \n\
+          :%%%%%%%%%%#- -: :*%%%%%%%%%%%%%%#+:.- +%%%%%*  \n\
+          .%%%%%=*%%%%%#: -%%%%%%%%%%%%%%%%%%%#+.=%%%%%*  \n\
+           %%%%%+ #%%%%%%= +%%%%%*=++*#%%%%%%%%%-=%%%%%*                Automates the proccess of\n\
+           #%%%%%-.#%%%%%%+ =%%%%#=:.    .-#%%%%=-%%%%%*                editing a three-camera set up.\n\
+           #%%%%%#.:#%%%%%%*.:#%%%%%%#*=+#%%%%%%=:%%%%%*  \n\
+           %%%%%%%* =%%%%%%%#:.*%%%%%%%%%%%%%%%%=:%%%%%*  \n\
+          :%%%%%%%%= +%%%%%%%#- =%%%%%%%%%%%%%%#-.%%%%%*  \n\
+          =%%%%%%%%%: *%%%%%%%%+ :#%%%%%#####**.  *****+\n\n\n\n\n";
+	for (char a : logoAscii) {
+		if (a == '%') {
+			std::cout << "\033[1;31m%\033[0m";
+		}
+		else {
+			std::cout << a;
+		}
+	}
+}
